ea6867d50d8237fcf36374d9fb2ec300825504ff
Laboratorio <laboratorio@udla-ec.int>
2026-04-28 11:34:19 -0500

    Ejercicios Cabeceras Jose Ortega

---
+++ OperacionesH__JoseOrtega.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char const* argv[])
+{
+int num1, num2;
+printf("Calculadora En C\n");
+printf("Ingrese el primer numero: ");
+scanf("%d", &num1 )
+printf("Ingrese el segundo numero: ");
+scanf("%d", &num2);
+sumar(a,b)
+return 0;    
+}
