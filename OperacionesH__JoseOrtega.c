36bf03a28e58810578f75bbba85291e7e8c223e8
Laboratorio <laboratorio@udla-ec.int>
2026-04-28 11:50:41 -0500

    OperacionesH_JoseOrtega

--- OperacionesH__JoseOrtega.c
+++ OperacionesH__JoseOrtega.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sumar.h>
 int main(int argc, char const* argv[])
 {
 int num1, num2;
@@ -7,6 +8,6 @@ printf("Ingrese el primer numero: ");
 scanf("%d", &num1 )
 printf("Ingrese el segundo numero: ");
 scanf("%d", &num2);
-sumar(a,b)
+printf("La suma es : %d",sumar(num1,num2));
 return 0;    
 }
---
+++ sumar.c
@@ -0,0 +1,2 @@
+int sumar(int num1, int num2 );
+return a+b
